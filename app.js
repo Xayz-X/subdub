c3675db4cd83b3ea9b5ff5f6edde52eb43fdfdc2
xZephy <xayzexx@gmail.com>
2025-09-10 03:36:35 +0530

    added urlencode & cookieParser middleware

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-import express  from "express";
+import express from "express";
 import { PORT } from "./config/env.js";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import connectToDatabase from "./database/database.js";
 import errorMiddleware from "./middlewares/error.middleware.js"
 
+// app initialize
 const app = express();
 
+// middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// auth router
+// routers
 app.use("/auth", authRouter)
 app.use("/users", userRouter)
 
+// custom error middleware
 app.use(errorMiddleware)
 
+// health check route
 app.get("/", (req, res) => {
     res.send("Hello world!")
 });
 
+// server start 
 app.listen(PORT, async () => {
     console.log(`Server is listening on: https://localhost:${PORT}`)
     await connectToDatabase()
